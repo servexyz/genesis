c2bac4da789a78007d63768c02c53012a68fc05d
alechp <me@alechp.com>
2018-02-21 17:29:55 -0800

    Added symlinks and configs

---
+++ config.js
@@ -0,0 +1,32 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    {
+      servexyz: "repo-genesis"
+    },
+    {
+      servexyz: "repo-genesis-cli"
+    },
+    {
+      servexyz: "file-genesis"
+    },
+    {
+      servexyz: "library-genesis"
+    },
+    {
+      servexyz: "content-genesis"
+    },
+    {
+      servexyz: "alpha-genesis"
+    },
+    {
+      servexyz: "library-genesis-cli"
+    },
+    {
+      servexyz: "library-genesis-example"
+    }
+  ]
+};
+
+module.exports = { config };
